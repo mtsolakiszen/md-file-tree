e8627b632d731d3f0fb277a208724243089a30d8
Michael Tsolakis <mtsolakis@zendesk.com>
2019-03-28 15:32:03 +1100

    make it alphabetical in the worst way possible

--- index.js
+++ index.js
@@ -15,18 +15,75 @@
 
   var parseResult = function(result){
     indentation++;
+    var outputs = []
+    var fileOutputs = [];
+    var dirOutputs = [];
     for (var i in result) {
       if (typeof result[i] === 'string' && i[0] !== '.') {
-        output += addIndentation() + '- [' + i + '](' + dir + result[i] + ')\n';
+        fileOutputs.push(addIndentation() + '- [' + i + '](' + dir + result[i] + ')\n');
       } else if (typeof result[i] === 'object'){
-        output += addIndentation() + '- __' + i + '__\n';
-        parseResult(result[i]);
+        dirOutputs.push(addIndentation() + '- __' + i + '__\n')
+
+
+        outputs.push({ [addIndentation() + '- __' + i + '__\n']: parseResult(result[i])});
         indentation--;
       }
     }
+    fileOutputs.sort((a, b) => a.toLowerCase() > b.toLowerCase());
+    var count = fileOutputs.length;
+    for (var j = 0; j < count; j++) {
+      outputs.push(fileOutputs.pop());
+    }
+
+    return outputs;
   };
   tree(dirPath, function(err, result){
-    parseResult(result);
+    var res = parseResult(result);
+
+    var sortFn = (a, b) => {
+      if(typeof(a) === 'object' && typeof(b) !== 'object') {
+        return true;
+      };
+      if(typeof(b) === 'object' && typeof(a) !== 'object') {
+        return false;
+      };
+      if(typeof(a) !== 'object' && typeof(b) !== 'object') {
+        return a.toLowerCase() > b.toLowerCase();
+      }
+      var first = Object.keys(a)[0].toLowerCase();
+      var second = Object.keys(b)[0].toLowerCase();
+      return first > second;
+    }
+
+    function sortResult(arrayOfResults) {
+      var newRes = [];
+      for(var i in arrayOfResults) {
+        if(typeof(arrayOfResults[i]) === 'object') {
+          var key = Object.keys(arrayOfResults[i])[0];
+          newRes.push({ [key]: sortResult(arrayOfResults[i][key]).sort(sortFn) })
+        }
+        else {
+          newRes.push(arrayOfResults[i]);
+        }
+      }
+      return newRes.sort(sortFn);
+    }
+
+    var finalRes = sortResult(res).sort(sortFn);
+
+    function addToOutput(arrayOfResults) {
+      for(var i in arrayOfResults) {
+        if(typeof(arrayOfResults[i]) === 'object') {
+          var key = Object.keys(arrayOfResults[i])[0];
+          output += key;
+          addToOutput(arrayOfResults[i][key]);
+        }
+        else {
+          output += arrayOfResults[i];
+        }
+      }
+    }
+    addToOutput(finalRes);
     console.log(output);
   });
 
